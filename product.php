f9596e5de419469e5c0a071e7990ec35113ffac4
lucasfuriofranco <lucasfuriofranco@gmail.com>
2015-04-22 17:23:13 -0300

    sec commit

--- product.php
+++ product.php
@@ -86,10 +86,54 @@
         <br><br>
         <?php if($product['availability']):?>
             <script>
-                function addProductToCart(){
-                    var cart_cookie = getCookie("brobath_cart_cookie");
-                    alert(cart_cookie);
+
+            function setCookie(cname,cvalue,exdays) {
+                var d = new Date();
+                d.setTime(d.getTime() + (exdays*24*60*60*1000));
+                var expires = "expires=" + d.toGMTString();
+                document.cookie = cname+"="+cvalue+"; "+expires;
+            }
+
+            function getCookie(cname) {
+                var name = cname + "=";
+                var ca = document.cookie.split(';');
+                for(var i=0; i<ca.length; i++) {
+                    var c = ca[i];
+                    while (c.charAt(0)==' ') {
+                        c = c.substring(1);
+                    }
+                    if (c.indexOf(name) == 0) {
+                        return c.substring(name.length, c.length);
+                    }
                 }
+                return "";
+            }
+
+            function checkCookie() {
+                var user=getCookie("username");
+                if (user != "") {
+                    alert("Welcome again " + user);
+                } else {
+                   user = prompt("Please enter your name:","");
+                   if (user != "" && user != null) {
+                       setCookie("username", user, 30);
+                   }
+                }
+            }
+
+            function Product(sku, ammount){
+                this.sku = sku;
+                this.amount = ammount;
+            }
+
+            function addProductToCart(){
+                var cart_cookie = getCookie("brobath_cart_cookie");
+
+
+
+                alert(cart_cookie);
+            }
+
             </script>
             <input value="comprar" id="add-to-cart-available" type="button" onclick=addProductToCart()>
 
